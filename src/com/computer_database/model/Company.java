3cc0c424fa6311d3d9126f452850dc529ffcd7d2
Camille Fadeuihe <cfadeuilhe@excilys.com>
2016-12-13 17:04:49 +0100

    Change setter equals hashcode, addition ComputerBuilder

--- src/com/computer_database/model/Company.java
+++ src/com/computer_database/model/Company.java
@@ -2,25 +2,65 @@ package com.computer_database.model;
 
 public class Company {
 
-	private int ID;
+	private long id;
 	private String name;
 
-	
-	
-	public Company(int ID, String name) {
-		super();
-		this.ID = ID;
+	public Company(long id, String name) {
+		this.id = id;
 		this.name = name;
 	}
-	public int getID() {
-		return ID;
+
+	public Company() {
+
 	}
+
+	public long getid() {
+		return id;
+	}
+
+	public void setid(int id) {
+		id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "\nCompany n°"+ID+" -> " + name;
+		return "\nCompany n°" + id + " -> " + name;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Company other = (Company) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
--- src/com/computer_database/model/Computer.java
+++ src/com/computer_database/model/Computer.java
@@ -1,87 +1,171 @@
 package com.computer_database.model;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.time.LocalDate;
 
 public class Computer {
-	
-	private int ID;
+
+	private long id;
 	private String name;
-	private int manufacturerID;
-	private String manufacturer;
-	private Date introductionDate;
-	private Date discontinutionDate;
-	
-	
-	public Computer(int ID,String name, int manufacturerID, String manufacturer, Date introductionDate, Date discontinutionDate) {
-		super();
-		this.ID = ID;
-		this.name = name;
-		this.manufacturer = manufacturer;
-		this.manufacturerID = manufacturerID;
-		this.introductionDate = introductionDate;
-		this.discontinutionDate = discontinutionDate;
+	private Company company;
+	private LocalDate introducedDate;
+	private LocalDate discontinuedDate;
+
+	private Computer(ComputerBuilder builder) {
+		this.id = builder.id;
+		this.name = builder.name;
+		this.company = builder.company;
+		this.introducedDate = builder.introducedDate;
+		this.discontinuedDate = builder.discontinuedDate;
 	}
-	
+
 	public Computer() {
-		super();
 	}
 
-	public Computer(int ID, String name) {
-		super();
-		this.ID = ID;
+	public Computer(int id, String name) {
+		this.id = id;
 		this.name = name;
-	}
-	
-	public void setComputer(int ID, String name,int manufacturerID,Date introductionDate,Date discontinutionDate){
-		this.ID = ID;
-		this.name = name;
-		this.manufacturerID = manufacturerID;
-		this.introductionDate = introductionDate;
-		this.discontinutionDate = discontinutionDate;
 	}
 
-	public int getID() {
-		return ID;
+	public void setComputer(long id, String name, Company company, LocalDate introducedDate,
+			LocalDate discontinuedDate) {
+		this.id = id;
+		this.name = name;
+		this.company = company;
+		this.introducedDate = introducedDate;
+		this.discontinuedDate = discontinuedDate;
 	}
-	public void setID(int ID) {
-		this.ID = ID;
+
+	public long getid() {
+		return id;
 	}
+
+	public void setid(long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getManufacturerID() {
-		return manufacturerID;
+
+	public Company getCompany() {
+		return company;
 	}
-	public void setManufacturerID(int manufacturerID) {
-		this.manufacturerID = manufacturerID;
-	}/*
-	public String getManufacturer() {
-		return manufacturer;
+
+	public void setCompany(Company company) {
+		this.company = company;
 	}
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}*/
-	public Date getIntroductionDate() {
-		return introductionDate;
+
+	public LocalDate getIntroducedDate() {
+		return introducedDate;
 	}
-	public void setIntroductionDate(Date introductionDate) {
-		this.introductionDate = introductionDate;
+
+	public void setIntroducedDate(LocalDate introducedDate) {
+		this.introducedDate = introducedDate;
 	}
-	public Date getDiscontinutionDate() {
-		return discontinutionDate;
+
+	public LocalDate getDiscontinuedDate() {
+		return discontinuedDate;
 	}
-	public void setDiscontinutionDate(Date discontinutionDate) {
-		this.discontinutionDate = discontinutionDate;
+
+	public void setDiscontinuedDate(LocalDate discontinuedDate) {
+		this.discontinuedDate = discontinuedDate;
 	}
-	
+
 	@Override
 	public String toString() {
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-		return "\nComputer n°"+ ID +": [Name=" + name + ", Manufacturer=" + ((manufacturer != null)?manufacturer:"Unknown manufacturer") + ", Introduced on=" + ((introductionDate != null)?df.format(introductionDate):"Date undefined")
-				+ ", Discontinued on=" + ((discontinutionDate != null)?df.format(discontinutionDate):"Date undefined") + "]";
+		return "\nComputer n°" + id + ": [Name=" + name + ", Company="
+				+ ((company != null) ? company.getName() : "Company unknown") + ", Introduced on="
+				+ ((introducedDate != null) ? df.format(introducedDate) : "Date undefined") + ", Discontinued on="
+				+ ((discontinuedDate != null) ? df.format(discontinuedDate) : "Date undefined") + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((discontinuedDate == null) ? 0 : discontinuedDate.hashCode());
+		result = prime * result + ((introducedDate == null) ? 0 : introducedDate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Computer other = (Computer) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (discontinuedDate == null) {
+			if (other.discontinuedDate != null)
+				return false;
+		} else if (!discontinuedDate.equals(other.discontinuedDate))
+			return false;
+		if (introducedDate == null) {
+			if (other.introducedDate != null)
+				return false;
+		} else if (!introducedDate.equals(other.introducedDate))
+			return false;
+		return true;
+	}
+
+	public static class ComputerBuilder {
+
+		private long id;
+		private String name;
+		private Company company;
+		private LocalDate introducedDate;
+		private LocalDate discontinuedDate;
+
+		public ComputerBuilder(String name) {
+			this.name = name;
+		}
+
+		public ComputerBuilder() {
+
+		}
+
+		public ComputerBuilder id(long id) {
+			this.id = id;
+			return this;
+		}
+
+		public ComputerBuilder name(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public ComputerBuilder company(Company company) {
+			this.company = company;
+			return this;
+		}
+
+		public ComputerBuilder introducedDate(LocalDate introducedDate) {
+			this.introducedDate = introducedDate;
+			return this;
+		}
+
+		public ComputerBuilder discontinuedDate(LocalDate discontinuedDate) {
+			this.discontinuedDate = discontinuedDate;
+			return this;
+		}
+
+		public Computer build() {
+			return new Computer(this);
+		}
 	}
 }
