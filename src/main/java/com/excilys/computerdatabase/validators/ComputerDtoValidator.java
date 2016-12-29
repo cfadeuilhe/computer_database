3843e6a1e7c57d26b300cddf670481b933fee62a
Camille Fadeuihe <cfadeuilhe@excilys.com>
2016-12-29 11:34:46 +0100

    [REFACTO] Validators

--- src/main/java/com/excilys/computerdatabase/validators/ComputerDtoValidator.java
+++ src/main/java/com/excilys/computerdatabase/validators/ComputerDtoValidator.java
@@ -8,41 +8,64 @@ import com.excilys.computerdatabase.dto.ComputerDto;
 import com.excilys.computerdatabase.util.Consts;
 
 public class ComputerDtoValidator {
-    private static List<String> list = new ArrayList<String>();
 
-    public static void idValidator(int id) {
+    /*
+     * Convention used : if return false -> error, true -> everything ok
+     * 
+     * */
+    
+    public static boolean idValidator(long id) {
         if (id <= 0) {
-            list.add(Consts.NEGATIVE_ID);
+            return false;
         }
+        return true;
     }
 
     public static void idValidator(String id) {
 
     }
 
-    public static void nameValidator(String name) {
+    public static boolean nameValidator(String name) {
         if (name == null || name.length() <= 0) {
-            list.add(Consts.NO_NAME);
-        } else if (name.length() <= 2) {
-            list.add(Consts.NAME_TOO_SHORT);
+            return false;
         }
+        return true;
+    }
+    
+    public static boolean nameLengthValidator(String name){
+        if (name.length() <= 2) {
+            return false;
+        }
+        return true;
     }
 
-    public static void dateValidator(String introducedDate, String discontinuedDate) {
+    public static boolean dateOrderValidator(String introducedDate, String discontinuedDate) {
         // Check if the introduced date is before the discontinued date
-        if (introducedDate != "" && discontinuedDate != "" && introducedDate != null && discontinuedDate != null) {
+        if (introducedDate != null && discontinuedDate != null && !introducedDate.isEmpty() && !discontinuedDate.isEmpty()) {
             if (LocalDate.parse(introducedDate).isAfter(LocalDate.parse(discontinuedDate))) {
-                list.add(Consts.DATE_ORDER_ERROR);
+                return false;
             }
         }
+        return true;
     }
 
     public static List<String> validate(ComputerDto c) {
-        // List<String> list = new ArrayList<String>();
-        list.clear();
+        List<String> list = new ArrayList<>();
 
-        ComputerDtoValidator.nameValidator(c.getName());
-        ComputerDtoValidator.dateValidator(c.getIntroducedDate(), c.getDiscontinuedDate());
+        if (c.getId() != 0) {
+            if (!ComputerDtoValidator.idValidator(c.getId())) {
+                list.add(Consts.NEGATIVE_ID);
+            }
+        }
+        if (!ComputerDtoValidator.nameValidator(c.getName())){
+            list.add(Consts.NO_NAME);
+        }
+        if (!ComputerDtoValidator.nameLengthValidator(c.getName())){
+            list.add(Consts.NAME_TOO_SHORT);
+        }
+        if (!ComputerDtoValidator.dateOrderValidator(c.getIntroducedDate(), c.getDiscontinuedDate())){
+            list.add(Consts.DATE_ORDER_ERROR);
+        }
 
         return list;
     }
--- src/main/java/com/excilys/computerdatabase/validators/ComputerValidator.java
+++ src/main/java/com/excilys/computerdatabase/validators/ComputerValidator.java
@@ -5,38 +5,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.excilys.computerdatabase.model.Computer;
+import com.excilys.computerdatabase.util.Consts;
 
 public class ComputerValidator {
 
-    private static List<String> list = new ArrayList<String>();
-	public static void idValidator(int id) {
-		if (id <= 0) {
-			list.add("NegativeId");
-		}
-	}
+    /*
+     * Convention used : if return false -> error, true -> everything ok
+     * 
+     */
 
-	public static void idValidator(String id) {
+    public static boolean idValidator(long id) {
+        if (id <= 0) {
+            return false;
+        }
+        return true;
+    }
 
-	}
+    public static void idValidator(String id) {
 
-	public static void nameValidator(String name){
-		if(name == null || name.length() <= 0){
-			list.add("NoName");
-		}else if(name.length() <= 2){
-			list.add("NameTooShort");
-		}
-	}
-	
-	public static void dateValidator(LocalDate date){
-	    
-	}
-	
-	public static List<String> validator(Computer c){
-	    //List<String> list = new ArrayList<String>();
-        list.clear();
+    }
 
-        ComputerDtoValidator.nameValidator(c.getName());
-        
+    public static boolean nameValidator(String name) {
+        if (name == null || name.length() <= 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean nameLengthValidator(String name) {
+        if (name.length() <= 2) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean dateOrderValidator(LocalDate introducedDate, LocalDate discontinuedDate) {
+        if (introducedDate != null && discontinuedDate != null) {
+            if (introducedDate.isAfter(discontinuedDate)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<String> validator(Computer c) {
+
+        List<String> list = new ArrayList<>();
+        if (c.getId() != 0) {
+            if (!ComputerValidator.idValidator(c.getId())) {
+                list.add(Consts.NEGATIVE_ID);
+            }
+        }
+        if (!ComputerValidator.nameValidator(c.getName())) {
+            list.add(Consts.NO_NAME);
+        }
+        if (!ComputerValidator.nameLengthValidator(c.getName())) {
+            list.add(Consts.NAME_TOO_SHORT);
+        }
+        if (!ComputerValidator.dateOrderValidator(c.getIntroducedDate(), c.getDiscontinuedDate())) {
+            list.add(Consts.DATE_ORDER_ERROR);
+        }
+
         return list;
-	}
+    }
 }
